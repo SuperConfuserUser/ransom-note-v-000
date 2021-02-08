96cf0a504bad644b85632e5a0d185d92f5cae7fe
Chely Ho <adept_inept@yahoo.com>
2021-02-08 11:41:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function buildHistogram(magazine) {
-  return magazine;
+  const histogram = {};
+  return magazine.forEach(l => {
+    if (historgram[l]) {
+      historgram[l]++;
+    } else {
+      historgram[l] = 1;
+    }
+  })
 }
 
 function canBuildNote(magazine, note) {
